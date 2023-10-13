bb29f21c28c94827c537bdd98f46e02abf5d3ce8
Kavinda Mecha <52319671+778569@users.noreply.github.com>
2023-10-13 13:35:21 +0530

    Add files via upload

---
+++ IEnumerable and IQueryable/IEnumerable and IQueryable/IEnum.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEnumerable_and_IQueryable
+{
+    public class IEnum
+    {
+        public void Test()
+        {
+            List<Student> studentList = new List<Student>()
+            {
+                new Student(){ID = 1, Name = "James", Gender = "Male"},
+                new Student(){ID = 2, Name = "Sara", Gender = "Female"},
+                new Student(){ID = 3, Name = "Steve", Gender = "Male"},
+                new Student(){ID = 4, Name = "Pam", Gender = "Female"}
+            };
+
+            IEnumerable<Student> students = from std in studentList
+                                            where std.Gender == "Male"
+                                            select std;
+
+            IQueryable<Student> students1 = (IQueryable<Student>)(from std in studentList // Should Add cast
+                                            where std.Gender == "Male"
+                                            select std);
+
+            foreach (var item in students)
+            {
+                Console.WriteLine("ID = {0} , Name = {1}, Gender = {2}",item.ID , item.Name, item.Gender);
+            }
+        }
+    }
+}
---
+++ IEnumerable and IQueryable/IEnumerable and IQueryable/Program.cs
@@ -0,0 +1,12 @@
+﻿using IEnumerable_and_IQueryable;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        IEnum New_Enum = new IEnum();
+        New_Enum.Test();
+
+        Console.ReadKey();  
+    }
+}
