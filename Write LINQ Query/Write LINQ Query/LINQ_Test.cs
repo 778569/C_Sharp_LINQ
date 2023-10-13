d205bd55e7c8b5f8a516b0ec1e07edd214cd7bc0
Kavinda Mecha <52319671+778569@users.noreply.github.com>
2023-10-13 10:50:00 +0530

    Add files via upload

---
+++ Write LINQ Query/Write LINQ Query/LINQ_Test.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Write_LINQ_Query
+{
+    public class LINQ_Test
+    {
+
+        public void Test()
+        {
+
+            //Data Source
+            List<int> integerList = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            //Query
+
+
+
+            var QuerySyntax = (from obj in integerList
+                               where obj > 5
+                               select obj).Sum();
+
+
+
+
+            Console.WriteLine($"Sum is : {QuerySyntax}");
+
+
+
+
+
+            
+
+
+            //var QuerySyntax = integerList.Where(x=>x > 5).ToList();
+
+            //foreach (var item in QuerySyntax)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+
+
+
+
+
+
+
+
+            //var QuerySyntax = from obj in integerList // Dats Source
+            //                  where obj > 5  // Condition
+            //                  select obj;   // Selection
+
+
+            //foreach (var item in QuerySyntax)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+        }
+    }
+}
---
+++ Write LINQ Query/Write LINQ Query/Program.cs
@@ -0,0 +1,14 @@
+﻿using Write_LINQ_Query;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Console.WriteLine("Hello, World!");
+
+        LINQ_Test lINQ_Test = new LINQ_Test();
+        lINQ_Test.Test();
+
+        Console.ReadKey();
+    }
+}
