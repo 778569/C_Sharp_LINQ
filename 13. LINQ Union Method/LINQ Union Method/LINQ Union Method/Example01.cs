f9c8f08e73c28018e169ababe82b84ddc194d093
Kavinda Mecha <52319671+778569@users.noreply.github.com>
2023-10-24 09:51:20 +0530

    Add files via upload

---
+++ 13. LINQ Union Method/LINQ Union Method/LINQ Union Method/Example01.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Union_Method
+{
+    public class Example01
+    {
+        public void Test()
+        {
+            List<int> dataSource1 = new List<int>() { 1, 2, 3, 4, 5, 6 };
+            List<int> dataSource2 = new List<int>() { 1, 3, 5, 8, 9, 10 };
+
+            var MS = dataSource1.Union(dataSource2).ToList();
+
+            var QS = (from ds in dataSource1
+                     select ds).Union(dataSource2).ToList();
+
+
+            foreach (var item in QS)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine();
+
+            foreach (var item in MS)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
---
+++ 13. LINQ Union Method/LINQ Union Method/LINQ Union Method/Program.cs
@@ -0,0 +1,12 @@
+﻿using LINQ_Union_Method;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Console.WriteLine("Hello, World!");
+
+        Example01 example01 = new Example01();
+        example01.Test();   
+    }
+}
