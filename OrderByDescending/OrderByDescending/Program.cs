8f36fecefbd9806668c639088ba0707218631f81
Kavinda Mecha <52319671+778569@users.noreply.github.com>
2023-10-26 16:26:07 +0530

    Add files via upload

---
+++ OrderByDescending/OrderByDescending/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Console.WriteLine("Hello, World!");
+
+        List<int> intList = new List<int>() { 10, 45, 35, 29, 100, 69, 58, 50 };
+
+        var OrderDes = intList.OrderByDescending(num=>num).ToList();
+
+        var OrderDesQS = (from num in intList
+                          orderby num descending
+                          select num).ToList();
+
+
+        foreach (var item in OrderDes)
+        {
+            Console.Write(item + " ");
+        }
+
+        Console.ReadKey();
+    }
+}
