3e5d6cfb7bb277a1b810ac2be8f0fe51f48970f6
Kavinda Bandara <52319671+778569@users.noreply.github.com>
2023-10-27 17:23:36 +0530

    Add files via upload

---
+++ LINQ Reverse Method/LINQ Reverse Method/Example01.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace LINQ_Reverse_Method
+{
+    public class Example01
+    {
+
+        public void Test()
+        {
+            int[] intArray = new int[] { 10, 30, 50, 40, 60, 20, 70, 100 };
+
+            var MS = intArray.Reverse();
+
+
+
+            var QS = (from list in intArray
+                      select list).Reverse();
+
+
+            foreach (var number in MS)
+            {
+                Console.Write(number + " ");
+            }
+        }
+
+
+
+        public void Test2()
+        {
+
+            List<string> stringList = new List<string>() { "Preety", "Tiwary", "Agrawal", "Priyanka", "Dewangan" };
+
+
+            IEnumerable<string> ReverseData = stringList.AsEnumerable().Reverse();
+
+            foreach (var name in ReverseData)
+            {
+                Console.Write(name + " ");
+            }
+
+            Console.WriteLine();
+
+            IQueryable<string> ReverseData2 = stringList.AsQueryable().Reverse();
+
+            foreach (var name in ReverseData2)
+            {
+                Console.Write(name + " ");
+            }
+
+
+
+
+
+
+
+
+
+
+
+            // var MS = stringList.Reverse();
+
+            //You cannot store the data like below as this method belongs to
+            //System.Collections.Generic namespace whose return type is void
+            //IEnumerable<int> ArrayReversedData = stringList.Reverse();
+
+
+            //stringList.Reverse();
+
+            //foreach (var name in stringList)
+            //{
+            //    Console.Write(name + " ");
+            //}
+        }
+    }
+}
---
+++ LINQ Reverse Method/LINQ Reverse Method/Program.cs
@@ -0,0 +1,14 @@
+﻿using LINQ_Reverse_Method;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Example01 example01 = new Example01();
+        //example01.Test();
+        example01.Test2();
+
+        Console.ReadKey();
+    }
+}
