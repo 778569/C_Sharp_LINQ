319aa3daf4fb7430192f3b80756dee2bb65e326a
Kavinda Bandara <52319671+778569@users.noreply.github.com>
2023-10-29 22:37:10 +0530

    Add files via upload

---
+++ 25.LINQ Count Method/LINQ_Count/LINQ_Count/LINQ_Count/Example01.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Count
+{
+    public class Example01
+    {
+
+        public void Test()
+        {
+            int[] intNumbers = new int[] { 60, 80, 50, 90, 10, 30, 70, 40, 20, 100 };
+            //Using Method Syntax
+            int MSCount = intNumbers.Where(num => num > 40).Count();
+            //Using Query Syntax
+            var QSCount = (from num in intNumbers
+                           where num > 40
+                           select num).Count();
+            Console.WriteLine("No of Elements = " + MSCount);
+
+        }
+    }
+}
---
+++ 25.LINQ Count Method/LINQ_Count/LINQ_Count/LINQ_Count/Program.cs
@@ -0,0 +1,17 @@
+﻿using LINQ_Count;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Console.WriteLine("Hello, World!");
+
+        //Example01 example01 = new Example01();
+        //example01.Test();
+
+        Example02 example02 = new Example02();
+        example02.Test();
+
+        Console.ReadKey();
+    }
+}
