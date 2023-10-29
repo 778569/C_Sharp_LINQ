ba9de8db243dc7b1b20453975a938d9c1ca95119
Kavinda Bandara <52319671+778569@users.noreply.github.com>
2023-10-29 16:59:17 +0530

    Add files via upload

---
+++ 23.LINQ Min Method/LINQ Min Method/LINQ Min Method/Example01.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Min_Method
+{
+    public class Example01
+    {
+
+        public void Test()
+        {
+            int LowSalaryMS = Employee.GetAllEmployees().Min(emp => emp.Salary);
+
+            int LowSalaryQS = (from emp in Employee.GetAllEmployees()
+                              select emp).Min(emp => emp.Salary);   
+
+            Console.WriteLine("Lowest Salary : " + LowSalaryQS);
+        }
+    }
+}
